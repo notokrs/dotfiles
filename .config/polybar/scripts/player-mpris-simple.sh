f6b50f5b24842213e79f636d575f27c83c8ded37
notokrs <rusnoto030@gmail.com>
2021-08-14 12:51:27 +0700

    update

--- .config/polybar/scripts/player-mpris-simple.sh
+++ .config/polybar/scripts/player-mpris-simple.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 player_status=$(playerctl status 2> /dev/null)
+artist=$(playerctl metadata artist 2> /dev/null)
+title=$(playerctl metadata title 2> /dev/null)
 smplayer_pid=$(pgrep -f smplayer)
 
 if [[ "$player_status" = "Playing" && -z $smplayer_pid ]]; then
-    echo  $(playerctl metadata artist) - $(playerctl metadata title)
+    echo $artist - $title | cut -c 1-50
 elif [[ "$player_status" = "Paused" && -z $smplayer_pid ]]; then
-    echo $(playerctl metadata artist) - $(playerctl metadata title)
+    echo $artist - $title | cut -c 1-50
 else
     echo ""
 fi
